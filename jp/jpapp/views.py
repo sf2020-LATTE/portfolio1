f7549c6135fa5cc4ffd140c9813e2facfb7ca82b
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-26 20:07:48 +0900

    サインアップ用viewの追加

--- jp/jpapp/views.py
+++ jp/jpapp/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login # 追加
+from django.contrib.auth.forms import UserCreationForm # 追加
+from django.shortcuts import render,redirect
 
 def index(request):
   return render(request, "jpapp/index.html")
 
 def home(request):
     return render(request, "jpapp/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("jpaap:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'jpapp/signup.html', context)
