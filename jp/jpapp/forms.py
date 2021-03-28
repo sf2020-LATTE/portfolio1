d93b8d450c308b529ecdd42be5f9db5c6fd8ce6f
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-28 15:51:40 +0900

    フォームの作成

--- jp/jpapp/forms.py
+++ jp/jpapp/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Company
 
 class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
         fields = ("username", "last_name", "first_name", "email",)
+
+class CompanyForm(forms.ModelForm):
+
+    class Meta:
+        model = List
+        fields = ("company_name",)
