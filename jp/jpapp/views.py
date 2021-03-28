80ec5366f72ff28ddaeee1be40a4d9b8c60c8b9d
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-28 14:27:15 +0900

    フォームの適用

--- jp/jpapp/views.py
+++ jp/jpapp/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from django.shortcuts import render,redirect, resolve_url
-from django.views.generic import DetailView, UpdateView 
+from django.views.generic import DetailView, UpdateView
+
+from .forms import UserForm
 
 def index(request):
   return render(request, "jpapp/index.html")
@@ -26,3 +28,15 @@ def signup(request):
         "form": form
     }
     return render(request, 'jpapp/signup.html', context)
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "jpapp/users/detail.html"
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "jpapp/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('jpapp:users_detail', pk=self.kwargs['pk'])
