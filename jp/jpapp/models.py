2a1165ddad9c15aebc1ef37b974fac0c903ed329
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-28 15:48:24 +0900

    companyモデル作成

--- jp/jpapp/models.py
+++ jp/jpapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    Company_name = models.CharField(max_length=200)
+    #ForeignKey は一対多を表現するリレーションシップ型
+    #CASCADE ユーザー削除時に同時にリストも削除
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Company_name
