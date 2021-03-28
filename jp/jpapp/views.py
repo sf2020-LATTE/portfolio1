2e03156acbd32c7151b2f4c66ca62195c43a3b83
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-28 13:27:22 +0900

    デコレータ適用

--- jp/jpapp/views.py
+++ jp/jpapp/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import login # 追加
-from django.contrib.auth.forms import UserCreationForm # 追加
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render,redirect
 
 def index(request):
