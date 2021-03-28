554c79754fc3c4567aebb87c4a587a37d4e0dd4b
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-28 16:55:09 +0900

    リスト一覧のurl設定

--- jp/jpapp/urls.py
+++ jp/jpapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'), 
     path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"), 
     path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"), 
+    path("companies/", views.CompanyListView.as_view(), name="companies_list"), 
     path("companies/create/", views.CompanyCreateView.as_view(), name="companies_create"), 
 ]
