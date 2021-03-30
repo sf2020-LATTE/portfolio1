47fd77aeb9d4ab84af222e57f2d36ef7cc3d2c97
sf2020-LATTE <second.s.fujii@gmail.com>
2021-03-30 20:35:26 +0900

    modelで選択肢と色分け設定

--- jp/jpapp/models.py
+++ jp/jpapp/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+CHOICE = (('light', 'document_screening'),
+          ('light', 'information_session'),
+          ('alert', 'first_interview'),
+          ('alert', 'second_interview'),
+          ('alert', 'third_interview'),
+          ('alert', 'fourth_interview'),
+          ('danger', 'pass'),
+          ('dark', 'failure'))
 
 class Company(models.Model):
     company_name = models.CharField(max_length=200)
@@ -9,6 +17,10 @@ class Company(models.Model):
     total_employee = models.IntegerField()
     establishd = models.DateField()
     description = models.TextField()
+    phase = models.CharField(
+      max_length = 50,
+      choices = CHOICE
+    )
 
     #ForeignKey は一対多を表現するリレーションシップ型
     #CASCADE ユーザー削除時に同時にリストも削除
@@ -16,13 +28,7 @@ class Company(models.Model):
 
     def __str__(self):
         return self.company_name
-        return self.location
-        return self.capital
-        return self.total_employee
-        return self.establishd
-        return self.description
 
-#テーブル名は複数形にするため、Todoからtaskに変更
 class Task(models.Model):
     task_title = models.CharField(max_length=100)
     task_memo = models.TextField()
