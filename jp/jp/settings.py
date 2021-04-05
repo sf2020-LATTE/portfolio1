4d5abb65fc129b92ba7392aeff6cacfa8c3a186a
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-05 23:04:16 +0900

    トップページ画像の表示
    staticファイル設定
    pillowインストール

--- jp/jp/settings.py
+++ jp/jp/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #サーバーで静的ファイルを管理する時に、どこにファイルを置くか決めるために設定
-STATIC_ROOT = os.path.join(BASE_DIR,'static') # 追加
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+#STATIC_ROOT = os.path.join(BASE_DIR,'static') # 追加
 #静的ファイルをどんなURLで公開するのかを決める
 STATIC_URL = '/static/'
 
