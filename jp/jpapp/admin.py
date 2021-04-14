bf0fec4da4dfbb507511e635924a02f82ff30c8e
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-14 21:51:30 +0900

    adminに操作性向上のオプション追加

--- jp/jpapp/admin.py
+++ jp/jpapp/admin.py
@@ -5,3 +5,9 @@ from django.contrib import admin
 from .models import Company
 
 admin.site.register(Company)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags', 'related_companies')
+
+admin.site.register(Company, CompanyAdmin)
