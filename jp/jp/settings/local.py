eaffb160478dd96a5a49bc3fc49c33a650b7ba79
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-20 15:29:27 +0900

    base.pyを作成、編集
    開発環境と本番環境の設定を分割するため
    base.pyをlocal.pyとproduction.pyに分割

---
+++ jp/jp/settings/local.py
@@ -0,0 +1,15 @@
+# settings/local.py
+
+from .base import *
+
+#デプロイ用設定
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'jw-1j!-!_g7v1ufh+g+umj55bmgh*6$@e8^+be#f!%h=limfr_'
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = []
