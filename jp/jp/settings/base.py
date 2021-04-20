e2378219639695e1280fea61b1ecb38f63d3fbb3
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-20 15:36:39 +0900

    SECRET_KEY 、 DEBUG 、 ALLOWED_HOSTSをbase.pyから削除

--- jp/jp/settings/base.py
+++ jp/jp/settings/base.py
@@ -20,22 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-
-#デプロイ用設定
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jw-1j!-!_g7v1ufh+g+umj55bmgh*6$@e8^+be#f!%h=limfr_'
-SECRET_KEY = env('SECRET_KEY')
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = env('DEBUG')
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
