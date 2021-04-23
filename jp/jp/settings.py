d948b1b8bcff5c5a37ba8c945376eeea85cc0d83
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-23 12:58:52 +0900

    コードの重複を解消
    SECRET_KEY 、DEBUG 、ALLOWED_HOSTSの変数定義が２箇所記載されていた。.envを読み込むように修正。

--- jp/jp/settings.py
+++ jp/jp/settings.py
@@ -21,21 +21,6 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECRET_KEY = 'jw-1j!-!_g7v1ufh+g+umj55bmgh*6$@e8^+be#f!%h=limfr_'
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-
-#デプロイ用設定
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jw-1j!-!_g7v1ufh+g+umj55bmgh*6$@e8^+be#f!%h=limfr_'
-SECRET_KEY = env('SECRET_KEY')
-
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = env('DEBUG')
