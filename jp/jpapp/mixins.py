31b98cabedc670898ee4b638ab00a9727f714eef
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-29 21:52:28 +0900

    calendar用のmixin導入

--- jp/jpapp/mixins.py
+++ jp/jpapp/mixins.py
@@ -6,3 +6,27 @@ class OnlyYouMixin(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
         return user.pk == self.kwargs['pk'] or user.is_superuser
+
+import calendar
+from collections import deque
+
+class BaseCalendarMixin:
+    """カレンダー関連Mixinの、基底クラス"""
+    first_weekday = 0  # 0は月曜から、1は火曜から。6なら日曜日からになります。お望みなら、継承したビューで指定してください。
+    week_names = ['月', '火', '水', '木', '金', '土', '日']  # これは、月曜日から書くことを想定します。['Mon', 'Tue'...
+
+    def setup_calendar(self):
+        """内部カレンダーの設定処理
+
+        calendar.Calendarクラスの機能を利用するため、インスタンス化します。
+        Calendarクラスのmonthdatescalendarメソッドを利用していますが、デフォルトが月曜日からで、
+        火曜日から表示したい(first_weekday=1)、といったケースに対応するためのセットアップ処理です。
+
+        """
+        self._calendar = calendar.Calendar(self.first_weekday)
+
+    def get_week_names(self):
+        """first_weekday(最初に表示される曜日)にあわせて、week_namesをシフトする"""
+        week_names = deque(self.week_names)
+        week_names.rotate(-self.first_weekday)  # リスト内の要素を右に1つずつ移動...なんてときは、dequeを使うと中々面白いです
+        return week_names
