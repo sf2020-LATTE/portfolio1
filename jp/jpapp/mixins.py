000244bbc0f6ee3ed8d8cfbc9f5c55e4b2dfdcbc
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-29 21:56:53 +0900

    カレンダー用mixinの導入

--- jp/jpapp/mixins.py
+++ jp/jpapp/mixins.py
@@ -30,3 +30,48 @@ class BaseCalendarMixin:
         week_names = deque(self.week_names)
         week_names.rotate(-self.first_weekday)  # リスト内の要素を右に1つずつ移動...なんてときは、dequeを使うと中々面白いです
         return week_names
+
+class MonthCalendarMixin(BaseCalendarMixin):
+    """月間カレンダーの機能を提供するMixin"""
+
+    def get_previous_month(self, date):
+        """前月を返す"""
+        if date.month == 1:
+            return date.replace(year=date.year-1, month=12, day=1)
+        else:
+            return date.replace(month=date.month-1, day=1)
+
+    def get_next_month(self, date):
+        """次月を返す"""
+        if date.month == 12:
+            return date.replace(year=date.year+1, month=1, day=1)
+        else:
+            return date.replace(month=date.month+1, day=1)
+
+    def get_month_days(self, date):
+        """その月の全ての日を返す"""
+        return self._calendar.monthdatescalendar(date.year, date.month)
+
+    def get_current_month(self):
+        """現在の月を返す"""
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        if month and year:
+            month = datetime.date(year=int(year), month=int(month), day=1)
+        else:
+            month = datetime.date.today().replace(day=1)
+        return month
+
+    def get_month_calendar(self):
+        """月間カレンダー情報の入った辞書を返す"""
+        self.setup_calendar()
+        current_month = self.get_current_month()
+        calendar_data = {
+            'now': datetime.date.today(),
+            'month_days': self.get_month_days(current_month),
+            'month_current': current_month,
+            'month_previous': self.get_previous_month(current_month),
+            'month_next': self.get_next_month(current_month),
+            'week_names': self.get_week_names(),
+        }
+        return calendar_data
