0106eb381b94919c8138bf1b2d6135cf8c6fb5f0
sf2020-LATTE <second.s.fujii@gmail.com>
2021-04-29 21:51:47 +0900

    scheduleモデルの作成

--- jp/jpapp/models.py
+++ jp/jpapp/models.py
@@ -116,3 +116,22 @@ class Comment(models.Model):
    created_at = models.DateTimeField('投稿日', default=timezone.now)
    def __str__(self):
        return self.text
+
+class Schedule(models.Model):
+    """スケジュール"""
+    interview_phase = models.IntegerField(
+      choices = CHOICE4,
+      default='0'
+    )
+    summary = models.CharField('概要', max_length=50)
+    description = models.TextField('詳細な説明', blank=True)
+    start_time = models.TimeField('開始時間', default=datetime.time(7, 0, 0))
+    end_time = models.TimeField('終了時間', default=datetime.time(7, 0, 0))
+    date = models.DateField('日付')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.summary
